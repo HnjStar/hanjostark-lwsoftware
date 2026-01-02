3da9efcbe2fd930b9ec99aecf796746bdae893c3
HnjStar <130159087+HnjStar@users.noreply.github.com>
2026-01-02 12:08:02 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'xxxxx'
-const supabaseAnonKey = 'xxxxxx'
+const supabaseUrl = 'https://bzshlphhsjwawuwikxxk.supabase.co'
+const supabaseAnonKey = 'sb_publishable_amjW7L7EWuW2zbuzQ-ZWFg_tDb0dRIR'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
